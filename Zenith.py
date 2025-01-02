2016a3686dd2cda40bd7d449585703de946e75b4
Brekkii <9214604@ecasd.us>
2025-01-01 23:29:39 -0600

    Update Zenith.py

---
+++ Zenith.py
@@ -0,0 +1,11 @@
+#Happy New Year 2025!
+
+print("Welcome to Zenith Finance.")
+
+#Make a loop!
+input("What is your currency?")
+Curr = "USD" "$"
+if Curr == "USD" or "$":
+    print("Proceeding forward")
+elif Curr == "":
+    print("Unsupported Currency.")
